dc0ce1c97441db9273515076b18366a853178156
Mariana Perez <marianasps003@gmail.com>
2023-06-19 15:06:15 -0600

    Conexión con la base de datos establecida

---
+++ models/usuario.js
@@ -0,0 +1,42 @@
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    name: {
+        type: String,
+        required: [true, "El nombre es obligatorio"]
+    },
+    last_name: {
+        type: String,
+        required: [true, "El apellido es obligatorio"]
+    },
+    email: {
+        type: String,
+        required: [true, "El email es obligatorio"],
+        unique: true
+    },
+    role: {
+        type: String,
+        required: true,
+        emun: ['EDITOR_ROLE', "AUTHOR_ROLE"]
+    },
+    occupation: {
+        type: String,
+        required: [true, "La ocupación es obligatoria"]
+    },
+    about_user: {
+        type: String,
+        required: [true, "La información sobre ti es obligatoria"]
+    },
+    img: {
+        type: String,
+        required: [true, "La fotografía es requerida"]
+    },
+    status: {
+        type: Boolean,
+        default: true
+    }
+});
+
+
+module.exports = model( 'Usuario', UsuarioSchema );
